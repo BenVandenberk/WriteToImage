2520b9e859f319f433d8aa8b894ed6bd730c8b71
BenVandenberk <Ben.Vandenberk@hotmail.com>
2017-12-23 04:32:09 +0100

    Start

---
+++ Afbeelding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SchrijvenOpAfbeelding
+{
+    public class Afbeelding
+    {
+        public string Path { get; set; }
+        public string Naam { get; set; }
+
+        public Afbeelding() {
+            this.Path = "N/A";
+            this.Naam = "";
+        }
+
+        public Afbeelding(string path, string naam) {
+            this.Path = path;
+            this.Naam = naam;
+        }
+
+        public override string ToString() {
+            return $"[{this.Naam}] {this.Path}";
+        }
+    }
+}
---
+++ StartPunt.cs
@@ -0,0 +1,7 @@
+﻿namespace SchrijvenOpAfbeelding
+{
+    public class StartPunt
+    {
+        
+    }
+}
